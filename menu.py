b7e5dc5bcc2d6954d47b6b8a532ecca5f7b6ff77
HiddenBlame <ruhiga92@gmail.com>
2024-11-25 16:18:57 +0100

    menu basico

--- menu.py
+++ menu.py
@@ -1,1 +1,8 @@
-print ("buena, avanzamos!")
+def mostrar_Menu():
+ print ("1- sumar")   
+ print ("2- restar")   
+ print ("3- multiplicar")   
+ print ("4- dividir")   
+ print ("5- salir")   
+
+numero = input("Ingresa un número")
